6e1881733c4bc9375a151a9ff376ee7e6bbbf8d6
markk87 <markk87@nycstudents.net>
2023-10-16 09:24:41 -0400

    sdfsdf

---
+++ DOM.js
@@ -0,0 +1,11 @@
+const DOMSelectors = {
+    form: document.querySelector('#form'),
+    firstName: document.querySelector(".first-name"),
+    h2: document.querySelectorAll("h2"),
+};
+
+DOMSelectors.form.addEventListener("submit", function(event) {
+    event.preventDeafult();
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2.forEach((el) => el.textContent = DOMSelectors.firstName.value)
+})
