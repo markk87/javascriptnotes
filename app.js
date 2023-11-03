15f832bce443bfcaa4a2207deea16e4656a94534
markk87 <markk87@nycstudents.net>
2023-11-03 09:21:44 -0400

    sdfsgf

--- app.js
+++ app.js
@@ -8,14 +8,25 @@
 // }
 
 
-function spaces(s, y, t) {
-    let total = 0;
-    for(let i = 0; i <= s; i++){
-        if(y[i] === "C" && t[i] === "C") {
-            total++;
-        }
+// function spaces(s, y, t) {
+//     let total = 0;
+//     for(let i = 0; i <= s; i++){
+//         if(y[i] === "C" && t[i] === "C") {
+//             total++;
+//         }
+//     }
+//     console.log(total);
+// }
+
+// spaces(5, "CC..C", ".CC..");
+
+function sequence(previous, next, end) {
+    for(let i = 0; i <= end; i++){
+        console.log(previous);
+        nextSeq = previous + next
+        previous = next
+        next = nextSeq
     }
-    console.log(total);
 }
 
-spaces(5, "CC..C", ".CC..");
+sequence(0, 1, 10);
