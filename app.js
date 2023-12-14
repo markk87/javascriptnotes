a1b9dc04fa75350b749cb7a69ba48c3f43ff0640
markk87 <markk87@nycstudents.net>
2023-12-14 09:23:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,20 +14,70 @@
 
 //REST API - Relation State Tranfer Application Programming Interface
 
-const URL = `https://api.quotable.io/random`;
+// const URL = `https://api.quotable.io/random`;
 
-async function getData(URL) {
-    try {
-        const response = await fetch(URL);
+// async function getData(URL) {
+//     try {
+//         const response = await fetch(URL);
         
-        console.log(response);
-        const data = await response.json();
-        console.log(data);
+//         console.log(response);
+//         const data = await response.json();
+//         console.log(data);
        
        
-    } catch (error) {
-        document.querySelector("h1").textContent = error;
-    }
+//     } catch (error) {
+//         document.querySelector("h1").textContent = error;
+//     }
     
+// }
+// getData(URL);
+
+
+const urmom = {
+    name: "Yourmom", //String
+    age: 16, //Integer
 }
-getData(URL);
+
+// let x = Array.from(urmom.name);
+// console.log(x);
+
+// // for(let i = 0; i < urmom.name.length; i++) {
+// //     console.log(urmom.name[i])
+// // }
+// let i = 0
+// while(i < urmom.name.length) {
+//     console.log(urmom.name[i]);
+//     i++
+// }
+
+// function needle(name, search){
+//     if(name.includes(search)){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(needle(urmom.name, "U"))
+
+// console.log(urmom.name)
+
+function sortString(name){
+    let x = [...name].sort();
+    console.log(x);
+}
+
+sortString("kdfjliaewuyruh3iwe7gywlijfhskdjfhsladkjfh")
+let x = [1,2,3];
+let y = [4,5,6];
+let z = [...x, ...y];
+x = [...x, ...y];
+console.log(x);
+
+function removeL(name) {
+    let x = Array.from(name);
+    x.splice(-1);
+    console.log(x);
+}
+
+removeL(urmom.name).forEach((el) => console.log(el))
